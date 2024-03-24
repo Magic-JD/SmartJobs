7969d282348151be032cf7c4757e07491af96ffe
MagicJoe <mag1ctr1cks@hotmail.com>
2024-03-24 16:48:55 +0700

    Makes generalized header/footer style.

--- src/main/java/org/smartjobs/com/Application.java
+++ src/main/java/org/smartjobs/com/Application.java
@@ -3,14 +3,8 @@ package org.smartjobs.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
-@EnableAsync
-@EnableScheduling
 @ConfigurationPropertiesScan
 public class Application {
 
