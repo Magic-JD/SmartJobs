dc7d59f22591338183b092efcfb7fdf11e6801ec
MagicJoe <mag1ctr1cks@hotmail.com>
2024-03-31 11:11:03 +0700

    Introducing ThymeleafConstants

---
+++ src/main/java/org/smartjobs/com/constants/ThymeleafConstants.java
@@ -0,0 +1,26 @@
+package org.smartjobs.com.constants;
+
+public class ThymeleafConstants {
+
+    private ThymeleafConstants() {
+    }
+
+    public static final String LANDING_PAGE = "index";
+    public static final String ANALYSIS_PAGE = "analyze";
+    public static final String CANDIDATE_PAGE = "candidates";
+    public static final String UPLOAD_PAGE = "upload";
+    public static final String ROLES_PAGE = "roles";
+
+
+    public static final String EMPTY_FRAGMENT = "empty-response";
+    public static final String CANDIDATE_TABLE_FRAGMENT = "table";
+    public static final String SINGLE_CANDIDATE_ROW_FRAGMENT = "single-candidate-row";
+
+
+    public static final String SCORING_FRAGMENT = "scoring";
+    public static final String RESULT_DETAILS_FRAGMENT = "result-details";
+    public static final String RESULT_COLLAPSED_FRAGMENT = "result-collapsed";
+
+
+    public static final String ROLE_FRAGMENT = "role";
+}
