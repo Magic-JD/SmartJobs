c57b6ff1cbce1e2f7b579c3a44afb836b268f38e
MagicJoe <mag1ctr1cks@hotmail.com>
2024-05-26 20:28:21 +0700

    Updating log aspect to show more information.

--- src/main/java/org/smartjobs/core/aspect/LogAspect.java
+++ src/main/java/org/smartjobs/core/aspect/LogAspect.java
@@ -1,27 +1,36 @@
 package org.smartjobs.core.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
 @Slf4j
+@Profile({"dev", "prod"})
 public class LogAspect {
 
     @Pointcut("execution(* org.smartjobs.core.ports.*.*.*(..))")
     private void portPlugs() {
     }
 
-    @Before(value = "portPlugs()")
-    public void logPortCalled(JoinPoint joinPoint) {
+    @Around(value = "portPlugs()")
+    public Object logPortCalledBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        String methodName = joinPoint.getSignature().getName();
         if (log.isDebugEnabled()) {
-            String name = joinPoint.getSignature().getName();
-            log.debug("Method {} called", name);
-
+            log.debug(">> {}() - {}", methodName, Arrays.toString(args));
         }
+        Object result = joinPoint.proceed();
+        if (log.isDebugEnabled()) {
+            log.debug("<< {}() - {}", methodName, result);
+        }
+        return result;
     }
 }
