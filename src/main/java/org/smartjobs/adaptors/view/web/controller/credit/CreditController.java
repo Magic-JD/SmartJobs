790963448ea2c6fbc1911cb0a840d1f9669d0fc3
MagicJoe <mag1ctr1cks@hotmail.com>
2024-07-05 07:44:00 +0700

    Change credit issuance redirect

--- src/main/java/org/smartjobs/adaptors/view/web/controller/credit/CreditController.java
+++ src/main/java/org/smartjobs/adaptors/view/web/controller/credit/CreditController.java
@@ -1,6 +1,7 @@
 package org.smartjobs.adaptors.view.web.controller.credit;
 
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.smartjobs.core.entities.User;
 import org.smartjobs.core.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.smartjobs.adaptors.view.web.constants.HtmxConstants.HX_REDIRECT;
+import static org.smartjobs.adaptors.view.web.constants.ThymeleafConstants.EMPTY_FRAGMENT;
 
 @Controller
 @RequestMapping("/credit")
@@ -25,9 +29,10 @@ public class CreditController {
 
     @HxRequest
     @PostMapping("/trial")
-    public String applyTrialCredits(@AuthenticationPrincipal User user, @RequestParam String code, Model model) {
+    public String applyTrialCredits(@AuthenticationPrincipal User user, @RequestParam String code, HttpServletResponse response, Model model) {
         long userId = user.getId();
         couponService.validateCoupon(userId, code);
-        return "credit/applied";
+        response.addHeader(HX_REDIRECT, "/");
+        return EMPTY_FRAGMENT;
     }
 }
